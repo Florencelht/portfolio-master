6dbdeb270db6b159b809c677b9f3a96d227ac786
Florencehli <115206373+Florencehli@users.noreply.github.com>
2024-10-23 17:52:34 -0700

    -add email

--- app/components/momo/CoolModeDemo.jsx
+++ app/components/momo/CoolModeDemo.jsx
@@ -1,11 +1,14 @@
 import { Button } from "../button/button";
 import { CoolMode } from "./CoolMode";
+import { Link } from 'react-router-dom';
  
 export function CoolModeDemo() {
   return (
     <div className="relative justify-center">
       <CoolMode>
-        <Button>Contact!</Button>
+        <Button>
+        <Link to="./contact" style={{ textDecoration: 'none', color: 'inherit' }}>Contact</Link>
+        </Button>
       </CoolMode>
     </div>
   );
